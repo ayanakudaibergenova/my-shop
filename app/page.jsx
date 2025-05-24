78b87066246ba47d4ebf14d5d7b177a9893fba92
Ayana Kudaibergenova <kudajbergenovaaana735@gmail.com>
2025-05-24 20:09:43 +0600

    feat:add page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,7 @@
-'use client'
+"use client";
 import React from "react";
 import HeaderSlider from "@/components/HeaderSlider";
 import HomeProducts from "@/components/HomeProducts";
-import Banner from "@/components/Banner";
 import NewsLetter from "@/components/NewsLetter";
 import FeaturedProduct from "@/components/FeaturedProduct";
 import Navbar from "@/components/Navbar";
@@ -11,7 +10,7 @@ import Footer from "@/components/Footer";
 const Home = () => {
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <div className="px-6 md:px-16 lg:px-32">
         <HeaderSlider />
         <HomeProducts />
