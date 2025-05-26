6dff0b53c4b2866779a60fb38494ea20a167efd5
Ayana Kudaibergenova <kudajbergenovaaana735@gmail.com>
2025-05-26 14:05:02 +0600

    feat:add Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,30 +10,42 @@ const Navbar = () => {
 
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700">
-      <Image
-        className="cursor-pointer w-28 md:w-32"
-        onClick={() => router.push("/")}
-        src={assets.logo}
-        alt="logo"
-      />
+      <span
+        className={
+          ("text-2xl text-pink-700 font-black tracking-wider uppercase hover:text-pink-500 hover-effect group font-sans",
+          )
+        }
+      >
+        Ayanit
+        <span
+          className={
+            ("text-pink-400 group-hover:text-pink-700 hover-effect", )
+          }
+        >
+          y
+        </span>
+      </span>
       <div className="flex items-center gap-4 lg:gap-8 max-md:hidden">
-        <Link href="/" className="hover:text-gray-900 transition">
-          Home
-        </Link>
-        <Link href="/all-products" className="hover:text-gray-900 transition">
-          Shop
-        </Link>
-        <Link href="/about" className="hover:text-gray-900 transition">
-          About Us
-        </Link>
-        <Link href="/contact" className="hover:text-gray-900 transition">
-          Contact
-        </Link>
+       <Link href="/" className="text-pink-500 transition">
+  Home
+</Link>
+<Link href="/all-products" className="text-pink-500 transition">
+  Shop
+</Link>
+<Link href="/about" className="text-pink-500 transition">
+  About Us
+</Link>
+<Link href="/contact" className="text-pink-500 transition">
+  Contact
+</Link>
+
+
 
         {isSeller && (
           <button
             onClick={() => router.push("/seller")}
-            className="text-xs border px-4 py-1.5 rounded-full"
+            className="text-xs border px-4 py-1.5 rounded-full text-pink-500"
+
           >
             Seller Dashboard
           </button>
@@ -42,7 +54,7 @@ const Navbar = () => {
 
       <ul className="hidden md:flex items-center gap-4 ">
         <Image className="w-4 h-4" src={assets.search_icon} alt="search icon" />
-        <button className="flex items-center gap-2 hover:text-gray-900 transition">
+        <button className="flex items-center gap-2 text-pink-500 hover:text-gray-900 transition">
           <Image src={assets.user_icon} alt="user icon" />
           Account
         </button>
@@ -57,7 +69,7 @@ const Navbar = () => {
             Seller Dashboard
           </button>
         )}
-        <button className="flex items-center gap-2 hover:text-gray-900 transition">
+        <button className="flex items-center gap-2 text-pink-500 hover:text-gray-900 transition">
           <Image src={assets.user_icon} alt="user icon" />
           Account
         </button>
